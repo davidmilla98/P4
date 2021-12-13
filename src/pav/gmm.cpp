8ffd08f5bf0d19e8d72d67ab3a3ef1e53745a316
davidmilla98 <92745238+davidmilla98@users.noreply.github.com>
2021-12-13 15:17:19 +0100

    DEBERES DIA 12 (Retraso 1 Día)

--- src/pav/gmm.cpp
+++ src/pav/gmm.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) Universitat Politècnica de Catalunya, Barcelona, Spain.
+/* Copyright (C) Universitat Politï¿½cnica de Catalunya, Barcelona, Spain.
  *
  * Permission to copy, use, modify, sell and distribute this software
  * is granted provided this copyright notice appears in all copies.
@@ -101,6 +101,7 @@ namespace upc {
   }
 
   /// \TODO Compute the logprob for the whole input data.
+  /// \DONE
   float GMM::logprob(const fmatrix &data) const {    
 
     if (nmix == 0 or vector_size == 0 or vector_size != data.ncol())
@@ -111,6 +112,8 @@ namespace upc {
 
     for (n=0; n<data.nrow(); ++n) {
       /// \TODO Compute the logprob of a single frame of the input data; you can use gmm_logprob() above.
+      /// \DONE
+      lprob += gmm_logprob(data[n]);
     }    
     return lprob/n;
   }
@@ -198,17 +201,24 @@ namespace upc {
 
   int GMM::em(const fmatrix &data, unsigned int max_it, float inc_threshold, int verbose) {
     unsigned int iteration;
-    float old_prob=-1e34, new_prob=-1e34, inc_prob=-1e34;
+    float old_prob=-1e34, new_prob=-1e34, inc_prob=-1e34; //Logprobabilidades
     
     fmatrix weights(data.nrow(), nmix);
     for (iteration=0; iteration<max_it; ++iteration) {
       /// \TODO
+      /// \DONE
 	  // Complete the loop in order to perform EM, and implement the stopping criterion.
 	  //
 	  // EM loop: em_expectation + em_maximization.
 	  //
       // Update old_prob, new_prob and inc_prob in order to stop the loop if logprob does not
       // increase more than inc_threshold.
+      new_prob = em_expectation(data, weights);
+      em_maximization(data, weights);
+
+      inc_prob = new_prob - old_prob;
+      old_prob = new_prob;
+
       if (verbose & 01)
 	cout << "GMM nmix=" << nmix << "\tite=" << iteration << "\tlog(prob)=" << new_prob << "\tinc=" << inc_prob << endl;
     }
